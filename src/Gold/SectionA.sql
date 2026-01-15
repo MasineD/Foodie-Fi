434969df2e0b0d8d6d81dd829ba17e49a2613faa
MasineD <masinedonald@gmail.com>
2026-01-15 10:25:00 +0200

    This rewrites the code to answer Section A questions with cleaned data, and work with the schemas

---
+++ src/Gold/SectionA.sql
@@ -0,0 +1,12 @@
+USE FoodieFiDB;
+/*********************** A.Customer Journey ***********************/
+SELECT TOP 20 s.CustomerID, s.StartDate,p.*
+FROM Silver.Subscriptions s
+JOIN Silver.Plans p
+ON s.PlanID = p.PlanID
+WHERE CustomerID IN (1,2,11,13,15,16,18,19)
+/*
+	Considering each customer's first subscription, it is evident that most- if not all, customers 
+	start with a free trial. Following the free trial, most of them upgrade to a basic monthly or pro 
+	monthly plan. Only a few churn or upgrade to pro annual plan.
+*/
